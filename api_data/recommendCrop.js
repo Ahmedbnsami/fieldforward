8d7194bd6c5951f69509f7032b462698082e6f81
ahmed.sami0306 <ahmedbnsami@gmail.com>
2025-08-28 14:55:50 +0200

    Fixing Recommend Crop Bug

--- api_data/recommendCrop.js
+++ api_data/recommendCrop.js
@@ -7,22 +7,11 @@ function recommendCrop(environment) {
 
   const suitableCrops = cropDatabase.filter(crop => {
     const tempMatch = temperature >= crop.tempRange[0] && temperature <= crop.tempRange[1];
-    const soilMatch = soilMoisture >= crop.soilMoistureRange[0] && soilMoisture <= crop.soilMoistureRange[1];
+    // const soilMatch = soilMoisture >= crop.soilMoistureRange[0] && soilMoisture <= crop.soilMoistureRange[1];
     const etMatch = evapotranspiration >= crop.evapotranspirationRange[0] && evapotranspiration <= crop.evapotranspirationRange[1];
-
-    let rainMatch = true;
-
-    // if irrigation is given, check effective water
-    if (typeof irrigation === "number") {
-      const effectiveWater = (precipitation || 0) + irrigation;
-      rainMatch = effectiveWater >= crop.rainfallRange[0] && effectiveWater <= crop.rainfallRange[1];
-    } 
-    // if no irrigation but rainfall is extremely low, ignore rainfall constraint
-    else if ((precipitation || 0) > 20) { 
-      rainMatch = precipitation >= crop.rainfallRange[0] && precipitation <= crop.rainfallRange[1];
-    }
-
-    return tempMatch && soilMatch && etMatch && rainMatch;
+    const rainMatch = irrigation >= crop.rainfallRange[0] && irrigation <= crop.rainfallRange[1];
+    
+    return tempMatch && etMatch && rainMatch;
   });
 
   return suitableCrops.length > 0 ?
